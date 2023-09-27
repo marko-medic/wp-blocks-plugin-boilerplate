230d7f4a5c4c36e485ea4b53d87166a6b652deb3
marko.medic <marko.medic@aws.talpanetwork.com>
2023-09-27 17:31:10 +0200

    Init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2626de60912e375485c7');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'dfaf405293cbb7a54f55');
